d66dc898f144a66733c93728e1f8eaf0603f3ae2
Erick <erick.dev.miller@gmail.com>
2023-05-30 00:48:07 +0500

    first commit

---
+++ MoveBuild/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace MoveBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string downloadPath = @"C:\Users\user\Downloads";
+
+            string pathToMove = @"D:\Amaya\builds";
+
+            string buildVersion, lastBuildPath, finalPath, appName;
+
+            string[] allBuilds = Directory.GetFiles(downloadPath);
+
+            lastBuildPath = TakeLastBuild(allBuilds);
+
+            appName = GetAppName(lastBuildPath, downloadPath);
+
+            Console.WriteLine(appName);
+
+            buildVersion = SetBuildVersion();
+
+            finalPath = CreatePath(appName, buildVersion, pathToMove);
+
+            Directory.CreateDirectory(finalPath);
+
+            File.Move(lastBuildPath, Path.Combine(finalPath, $"{appName}.ipa"));
+
+            Process.Start("explorer.exe", finalPath);
+        }
+
+        static string TakeLastBuild(string[] allBuilds)
+        {
+            string lastBuild;
+
+            do
+            {
+                Array.Sort(allBuilds, (a, b) => new FileInfo(b).LastWriteTime.CompareTo(new FileInfo(a).LastWriteTime));
+
+            } while (!allBuilds[0].EndsWith(".ipa"));
+
+            lastBuild = allBuilds[0];
+
+            return lastBuild;
+        }
+
+        static string GetAppName(string buildPath, string path)
+        {
+            buildPath = buildPath.Replace($@"{path}\", "");
+            string[] separatedBuildName = buildPath.Split('(');
+
+            buildPath = separatedBuildName[0];
+            buildPath = buildPath.TrimEnd();
+
+            return buildPath;
+        }
+
+        static string SetBuildVersion()
+        {
+            string buildVersion;
+            Console.WriteLine("Введите номер билда");
+
+            buildVersion = Console.ReadLine();
+
+            if (!buildVersion.Any(char.IsDigit))
+            {
+                SetBuildVersion();
+            }
+
+            return buildVersion;
+        }
+
+        static string CreatePath(string appName, string version, string pathToMove)
+        {
+            string finalPath = $@"{pathToMove}\{appName}\{version}";
+
+            return finalPath;
+        }
+    }
+}
---
+++ MoveBuild/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MoveBuild")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoveBuild")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5a5ff630-8a0a-4fca-9e19-9cd1fa68f355")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
